6e6f7abd68d7351f64ca760c296f7f4020ef8bf8
MariaLanss <maruska0990@gmail.com>
2023-06-23 21:34:21 +0300

    .

---
+++ src/main/java/pro/sky/hw11_cource2/Hw11Cource2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.hw11_cource2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw11Cource2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw11Cource2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/hw11_cource2/Hw11Cource2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw11_cource2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw11Cource2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
